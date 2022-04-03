d161bafe1c29f2098330457d652b6d02c53f369a
Louis Y <louisy603@hotmail.com>
2022-04-03 00:29:09 -0400

    adding mouseover event

--- script.js
+++ script.js
@@ -2,3 +2,13 @@ const h2 = document.createElement("h2");
 h2.textContent = "this content added by Javascript";
 
 document.querySelector("body").appendChild(h2);
+
+
+function setNewImage() {
+    document.getElementById("imgMe").src = "sunset-2.jpg"
+}
+
+function setOldImage() {
+    document.getElementById("imgMe").src = "sunset-1.jpg"
+
+}
